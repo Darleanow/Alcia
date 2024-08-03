215c366c51a479b357e523aca6d3c8cd537227fa
Enzo Hugonnier <enzohugonn@gmail.com>
2024-08-03 14:22:57 +0200

    feat(Main): Add handling of the options
    Signed-off-by: Enzo Hugonnier <enzohugonn@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include <memory>
 #include <string>
-#include "ftxui/component/captured_mouse.hpp"
+#include <iostream>
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/screen/color.hpp"
 
 using namespace ftxui;
+
+/**
+ * Sets up the game
+ */
+void play_game() {
+    std::cout << "Starting the game..." << std::endl;
+    // Add your game starting logic here
+}
+
+/**
+ * Displays some parameters to configure for an enhanced experience
+ */
+void show_options() {
+    std::cout << "Displaying options..." << std::endl;
+    // Add your options logic here
+}
+
+/**
+ * Some informations about the dev, project related.
+ */
+void show_about() {
+    std::cout << "Showing about information..." << std::endl;
+    // Add your about logic here
+}
+
+/**
+ * Exit the game.
+ */
+void quit_game() {
+    exit(0);
+}
 
 int main() {
     auto screen = ScreenInteractive::Fullscreen();
@@ -20,19 +51,38 @@ int main() {
         L"Quit"
     };
     int selected = 0;
-    auto menu = Menu(&entries, &selected) | color(Color::Orange1) | border;
 
-    const auto container = Container::Vertical({
+    auto menu = Menu(&entries, &selected);
+
+    auto container = Container::Vertical({
         menu
     });
 
-    const auto renderer = Renderer(container, [&] {
+    auto main_component = Renderer(container, [&] {
         return vbox({
                    text(title) | bold | color(Color::Orange3) | border,
-                   menu->Render()
+                   menu->Render() | color(Color::Orange1) | border
                }) | center | borderHeavy;
     });
 
-    screen.Loop(renderer);
+    main_component = CatchEvent(main_component, [&](const Event &event) {
+        if (event == Event::Return) {
+            switch (selected) {
+                case 0: play_game();
+                    break;
+                case 1: show_options();
+                    break;
+                case 2: show_about();
+                    break;
+                case 3: quit_game();
+                    break;
+                default: break;
+            }
+            return true;
+        }
+        return false;
+    });
+
+    screen.Loop(main_component);
     return 0;
 }
