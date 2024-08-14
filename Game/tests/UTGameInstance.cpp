0f96777a2856a9943b39802cfe2b69975274b7f1
Enzo Hugonnier <enzohugonn@gmail.com>
2024-08-14 17:44:20 +0200

    fea(UnitTesting): Setup gtest with useless tests for now

---
+++ Game/tests/UTGameInstance.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(UTGameInstance, test_gtesta) {
+  EXPECT_EQ(1,1);
+  EXPECT_LT(1,2);
+}
---
+++ Player/tests/UTPlayer.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(UTPlayer, test_gtestb) {
+  EXPECT_EQ(1,1);
+  EXPECT_LT(1,2);
+  EXPECT_GT(2,1);
+}
